1551bb048e2f55f4b4bb95766f572a245b95402a
Chen Gong <chen.sst@gmail.com>
2021-01-15 00:46:48 +0800

    fix(grammar_module): define module group "octagram" as entry module
    when the plugin is loaded as shared library, the module group named
    after the plugin library is initialized, it then loads the contained
    module "grammar".

--- src/grammar_module.cc
+++ src/grammar_module.cc
@@ -6,6 +6,7 @@
 #include "octagram.h"
 #include <rime/common.h>
 #include <rime/registry.h>
+#include <rime/setup.h>  // for rime::LoadModules in RIME_REGISTER_MODULE_GROUP
 #include <rime_api.h>
 
 static void rime_grammar_initialize() {
@@ -19,3 +20,5 @@ static void rime_grammar_initialize() {
 static void rime_grammar_finalize() {}
 
 RIME_REGISTER_MODULE(grammar)
+
+RIME_REGISTER_MODULE_GROUP(octagram, "grammar")
