c791e8403d41dbf041ed98939b4cd0f7cc0d677b
居戎氏 <chen.sst@gmail.com>
2024-02-05 21:18:12 +0800

    fix build error

--- tools/build_grammar.cc
+++ tools/build_grammar.cc
@@ -27,11 +27,11 @@ int main(int argc, char* argv[]) {
   std::sort(data.begin(), data.end());
 
   GramDb db(language + kGramDbType.suffix);
-  LOG(INFO) << "creating " << db.file_name();
+  LOG(INFO) << "creating " << db.file_path();
   if (!db.Build(data) || !db.Save()) {
-    LOG(ERROR) << "failed to build " << db.file_name();
+    LOG(ERROR) << "failed to build " << db.file_path();
     return 1;
   }
-  LOG(INFO) << "created: " << db.file_name();
+  LOG(INFO) << "created: " << db.file_path();
   return 0;
 }
